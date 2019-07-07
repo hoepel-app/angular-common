71c6bf437ef55069d4a2a4d44039b2d78bb47598
Thomas Toye <thomas@toye.io>
2019-07-07 20:46:27 +0200

    fix: remove unused import

--- projects/ng-hoepel-common-demo/src/app/app.component.ts
+++ projects/ng-hoepel-common-demo/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Tenant } from '@hoepel.app/types';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
